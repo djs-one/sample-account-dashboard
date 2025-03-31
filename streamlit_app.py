536e487f4686a45b1eceda480c8f21c18db938f6
Daniel Stadt <95497733+djs-one@users.noreply.github.com>
2025-03-31 01:42:12 +0000

    Added code to read in example.xlsx file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
+import os
+
+import pandas as pd
 import streamlit as st
 
-st.title("🎈 My new app")
+def get_data():
+    xl = pd.ExcelFile("data/example.xlsx")
+    sheets = xl.sheet_names
+    dfd = {sheet: pd.read_excel(xl, sheet).assign(account=sheet[-1]) for sheet in sheets}
+    df = pd.concat(dfd, ignore_index=True)
+
+    return df
+
+st.title("Sample Dashboard")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
