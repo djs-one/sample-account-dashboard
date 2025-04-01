1b095039e22226f1477c16c1a756c90ec7fd0212
dan-stadt <dstadt14@gmail.com>
2025-03-31 20:37:24 -0400

    Updated get_data to remove redundant columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,25 @@ import plotly.express as px
 
 
 def get_data():
+    """Get account data"""
     xl = pd.ExcelFile("data/example.xlsx")
     sheets = xl.sheet_names
-    dfd = {sheet: pd.read_excel(xl, sheet).assign(account=sheet[-1]) for sheet in sheets}
-    df = pd.concat(dfd, ignore_index=True)
-    df["DateTime"] = pd.to_datetime(df["DateTime"])
+    dfd = {sheet: pd.read_excel(xl, sheet, index_col="DateTime", parse_dates=True) for sheet in sheets}
+    df = pd.concat(dfd, axis=1)
+
+    accts = df.columns.get_level_values(0).unique()
+    cols = df.columns.get_level_values(1).unique()
+
+    out = pd.DataFrame(index=df.index)
+    for col in cols:
+        try:
+            acct1, acct2 = accts[0], accts[1]
+            pd.testing.assert_series_equal(df[acct1][col], df[acct2][col])
+            out[col] = df[acct1][col]
+        except AssertionError:
+            for acct in accts:
+                out[f"{acct}-{col}"] = df[acct][col]
+
     return df
 
 df = get_data()
