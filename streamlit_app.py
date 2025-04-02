e56e63cc00805e4e7439022fbc798f5792e9ff51
dan-stadt <dstadt14@gmail.com>
2025-04-01 23:12:33 -0400

    Switched to altair chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,60 @@
 import os
 
+import altair as alt
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 
 def get_data():
     """Get account data"""
+    # Read each column into a DataFrame dictionary
+    dfd = {}
     xl = pd.ExcelFile("data/example.xlsx")
+
+    # Read each tab
     sheets = xl.sheet_names
-    dfd = {sheet: pd.read_excel(xl, sheet, index_col="DateTime", parse_dates=True) for sheet in sheets}
-    df = pd.concat(dfd, axis=1)
+    for sheet in sheets:
+        df = pd.read_excel(xl, sheet, index_col="DateTime", parse_dates=True)
+        for col in df.columns:
+            out = df[col]
+            out.name = sheet
 
-    accts = df.columns.get_level_values(0).unique()
-    cols = df.columns.get_level_values(1).unique()
+            # Check if column has been added to the dfd
+            if dfd.get(col) is not None:
 
-    out = pd.DataFrame(index=df.index)
-    for col in cols:
-        try:
-            acct1, acct2 = accts[0], accts[1]
-            pd.testing.assert_series_equal(df[acct1][col], df[acct2][col])
-            out[col] = df[acct1][col]
-        except AssertionError:
-            for acct in accts:
-                out[acct] = df[acct][col]
+                # If added, confirm new values in that column
+                if out[dfd.get(col) != out].any():
+                    dfs = [dfd[col], out]  # DataFrame list
+                    for i, _sr in enumerate(dfs):
+                        name = _sr.name.split("-")
+                        _df = _sr.to_frame()
+                        _df[name[0]] = name[1]
+                        _df = _df.rename(columns={_sr.name: col})
+                        dfs[i] = _df
+                    out = pd.concat(dfs)
+                    
 
-    return df
+                # Reset names in duplicated series
+                else:
+                    out.name = col
+                    out = out.reset_index()
+            dfd[col] = out
 
+    return dfd
+
+def get_figs(dfd):
+    #fig = make_subplots(rows=3, cols=3)
+    for i, val in enumerate(["Spot Price", "Temperature"]):
+    #    fig.add_trace(px.line(dfd[val], x="DateTime", y=val), col=i, row=1)
+        chart = alt.Chart(dfd[val]).mark_line().encode(x="DateTime", y=val)
+        st.altair_chart(chart)
 
 if __name__ == "__main__":
-    df = get_data()
-
     st.title("Sample Dashboard")
+    st.header("Accounts")
 
-    # Create checkboxes to 
-    st.write("Accounts")
-
-    cols = df.columns[1:-1]
-    for col in cols:
-        st.header(col)
-        fig = px.line(df, x='DateTime', y=col, color='account')
-        st.plotly_chart(fig)
-
+    dfd = get_data()
+    figs = get_figs(dfd)
